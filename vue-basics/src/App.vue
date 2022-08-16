b263ad64239a179cee0eb6b792cd6a3ab26f6060
bogdinamitawhoami <bogdifromsalem@yahoo.com>
2022-08-16 21:05:51 +0200

    Iteration 1 done

---
+++ vue-basics/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <NaviComp />
+
+  <router-view/>
+  <FooterComp />
+</template>
+
+<script>
+import { ref } from 'vue';
+import FooterComp from './components/FooterComp.vue';
+import NaviComp from './components/NaviComp.vue';
+
+export default {
+  name: 'App',
+  components: {
+    FooterComp, NaviComp,
+  },
+  setup() {
+    const name = ref(0);
+    return {
+      name,
+    };
+  },
+};
+
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+</style>
---
+++ vue-basics/src/components/FooterComp.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer>
+    I am the footer
+  </footer>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+footer {
+  background-color: #42b983;
+  padding: 200px 30px;
+  color: white;
+}
+</style>
